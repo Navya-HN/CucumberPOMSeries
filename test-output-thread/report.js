898c45b6d8139e26048527d7d5beb63875074fa2
Navya-HN <navya992@gmail.com>
2022-07-05 11:14:41 +0200

    This is my first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "fc348660-2cf8-48fe-8739-a845eef82e13",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1656883036916,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1656883122432,
+    "className": "passed"
+  },
+  {
+    "id": "e9e75cd8-3edf-4bba-a0f0-960e11f10a7a",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1656883122468,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1656883155679,
+    "className": "failed"
+  },
+  {
+    "id": "8335641f-6521-4a9a-80c1-d45afc9a02f2",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1656883037045,
+    "group": 17,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1656883118239,
+    "className": "passed"
+  },
+  {
+    "id": "2bacbc19-9754-4694-88b0-daf5130356f8",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1656883118283,
+    "group": 17,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1656883155824,
+    "className": "passed"
+  },
+  {
+    "id": "387e7dd8-48c1-43f9-982f-7f5b10649a44",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1656883155702,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1656883177310,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 17,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[pool-1-thread-2,5,main]"
+  }
+]);
+});
